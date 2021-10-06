85ae6a85c76ed912f323fa644f243252fe70a8fe
Octavian Sirbu <octavian.sirbu@ibm.com>
2021-10-06 10:59:55 +0300

    initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+export const mount = (el) => {
+    ReactDOM.render(
+        <App />,
+        el
+    );
+}
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.getElementById('_marketing-dev-root');
+
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
